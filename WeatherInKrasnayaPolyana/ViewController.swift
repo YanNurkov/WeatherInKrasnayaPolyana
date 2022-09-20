57425ecffc7d25d03250fe7c9832d31ae8b56f43
YanNurkov <85751034+YanNurkov@users.noreply.github.com>
2022-09-20 10:59:12 +0300

    Initial Commit

---
+++ WeatherInKrasnayaPolyana/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherInKrasnayaPolyana
+//
+//  Created by Ян Нурков on 20.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
